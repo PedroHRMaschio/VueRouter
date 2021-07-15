f3749af78d6fbe6b3018894be94faa4cfd0798eb
PedroHRMaschio <pedrohenriquermaschio@gmail.com>
2021-07-15 14:18:19 -0300

    rota cadastro teste

---
+++ routerguia/src/views/Cadastro.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <input type="text" name="email" placeholder="email@teste.com"><br>
+        <input type="text" name="nome" placeholder="Digite aqui seu nome"><br>
+        <button>Teste!</button>
+    </div>
+</template>
